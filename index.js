3b73ace58d575ef4938a05f4a6ebb2f0674476f0
Anthony H <32407457+xecraft@users.noreply.github.com>
2018-08-16 11:45:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 var client = new Discord.Client();
 
-bot.on("ready", function () {
-    bot.user.setGame("", "https://www.twitch.tv/austelengine")
+client.on("ready", function () {
+    client.user.setGame("", "https://www.twitch.tv/austelengine")
     console.log("AustelBot V3 - Connecté");
 });
 
-bot.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
