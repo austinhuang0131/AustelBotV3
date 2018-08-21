f3f39847a226bcdf9ffb25c41e7d7b0f4d0a2b2a
Anthony H <32407457+anthonyholstein@users.noreply.github.com>
2018-08-21 18:00:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,95 @@
 const Discord = require("discord.js");
-const DiscordRPC = require("discord-rpc");
 var client = new Discord.Client();
 const ClientId = '479672416574898177';
-
-DiscordRPC.register(ClientId);
-
-const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-
-rpc.on('ready', () => {
-  console.log("ready");
-  rpc.setActivity({
-    details: `test`,
-    state: 'test',
-    // largeImageKey: 'test',
-    // largeImageText: 'test',
-    // smallImageKey: 'test',
-    // smallImageText: 'test',
-    instance: false,
-  });
-});
-
-rpc.login(ClientId).catch(console.error);
-
 var prefix = "A!";
+
+const serverStats = {
+  guildID: '439522665800138752',
+  totalUsersID: '481578470502170624',
+  memberCountID: '481580084105576468',
+  botCountID: '481580212392427531'
+};
 
 client.on("ready", function () {
  //   client.user.setGame("", "https://www.twitch.tv/austelengine")
     console.log("AustelBot V3 - Connecté");
+});
+
+client.on("guildMemberAdd", function(member) {               
+    member.addRole(member.guild.roles.find("name", ">! Membre !<"));
+    var games = [
+    "A!help | AUSTELBOT V1.2",
+    "Développé par XeCrafT",
+    "http://austelclient.net",
+    " " + new Date(),
+     client.users.size + " utilisateurs !"
+ ]
+  client.user.setActivity(setInterval(function() {
+  client.user.setActivity(games[Math.floor(Math.random() * games.length)], {url:"https://www.twitch.tv/AustelBot", type: "STREAMING"})
+  }, 3000))
+  
+  if(member.guild.id !== serverStats.guildID) return;
+  
+  client.channels.get(serverStats.totalUsersID).setName(`Membres totals : $(member.guild.memberCount}`);
+  client.channels.get(serverStats.memberCountID.setName(`Membres : ${member.guild.members.filter(m => !m.user.bot).size}`);
+  client.channels.get(serverStats.botCountID.setName(`Robots : ${member.guild.members.filter(m => m.user.bot).size}`);
+  
+ member.guild.channels.find("name", "austel-chat").sendMessage("", {    
+            embed: {
+                color: 0x008000,
+                author: '',
+                title: '', 
+                description: '', 
+                fields: [
+                    {
+                        name: member.displayName,
+                        value: 'a rejoint :white_check_mark: ',
+                        inline: false
+                   }],                     
+                                   footer: {
+            text: 'Bievenue à toi ! :D',
+          },
+            }
+ });
+});
+
+client.on("guildMemberRemove", function(member) {
+    
+    var games = [
+    "A!Help | AUSTELBOT V2.0",
+    "Développé par XeCrafT",
+    "http://austelclient.net",
+    " " + new Date(),
+     client.users.size + " utilisateurs !"
+ ]
+  client.user.setActivity(setInterval(function() {
+  client.user.setActivity(games[Math.floor(Math.random() * games.length)], {url:"https://www.twitch.tv/AustelBot", type: "STREAMING"})
+  }, 3000))
+
+  if(member.guild.id !== serverStats.guildID) return;
+  
+  client.channels.get(serverStats.totalUsersID).setName(`Membres totals : $(member.guild.memberCount}`);
+  client.channels.get(serverStats.memberCountID.setName(`Membres : ${member.guild.members.filter(m => !m.user.bot).size}`);
+  client.channels.get(serverStats.botCountID.setName(`Robots : ${member.guild.members.filter(m => m.user.bot).size}`);
+    
+ member.guild.channels.find("name", "austel-chat").sendMessage("", {    
+            embed: {
+                color: 0xFF0000,
+                author: '',
+                title: '', 
+                description: '', 
+                fields: [
+                    {
+                        name: member.displayName,
+                        value: 'a quitté le serveur :x:',
+                        inline: false
+                   }],                     
+                                   footer: {
+            text: 'À Bientôt !',
+          },
+            }
+ });
+    
 });
 
 client.on('message', message => {
